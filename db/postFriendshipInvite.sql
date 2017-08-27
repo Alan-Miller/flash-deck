539379a75525e17f5505cdf6cf4761e154ab19f9
Alan-Miller <alan.ashman@gmail.com>
2017-08-27 02:39:54 -0700

    cleaned up sql statement

--- db/postFriendshipInvite.sql
+++ db/postFriendshipInvite.sql
@@ -4,23 +4,6 @@ where not exists (select * from friendships
          where (inviter_id = $1 and invitee_id = $2) 
             or (inviter_id = $2 and invitee_id = $1));
 
--- select display_name from friendships
--- join users
--- on users.id = friendships.inviter_id or users.id = friendships.invitee_id
--- where users.id != $1
---     and (inviter_id = $1 or invitee_id = $1)
---     and friendship_formed = FALSE;
-
-
--- $insert = "insert into friendships (inviter_id, invitee_id) select ($1, $2)";
--- $upsert = "update friendships set invitee_id = $2 
--- where invitee_id = $1 and invitee_id = $2";
--- with upsert as ($upsert returning *) $insert where not exists (select * from upsert);
-
--- insert into friendships (inviter_id, invitee_id) 
--- values ($1, $2)
--- on conflict do nothing;
-
 -- UPDATE
 -- update friendships
 -- set inviter_id = 2, invitee_id = 1
